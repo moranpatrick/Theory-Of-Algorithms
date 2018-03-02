5024508f70ec695dbe2f12cb616e3fc84dc81c1e
moranpatrick <patrickmoran121@gmail.com>
2018-03-02 17:48:01 +0000

    Used cond to have an extra check for a positive number - Updated README.md

--- Tasks/Task2/Task2.rkt
+++ Tasks/Task2/Task2.rkt
@@ -4,15 +4,19 @@
   (if (even? n)
       ; If the number is even perform this equation
       (/ n 2)
-      ; Otherwise perform This equation
+      ; Otherwise perform this equation
       (+ 1 (* 3 n))))
 
 ; Function called collatz-list which returns a list by recursively
 ; performing an operation depending on if the input is odd or even
 (define (collatz-list n)
-    (if (= n 1)
-        (cons n '())
-        (cons n(collatz-list (even-odd n)))))
+  (cond
+    ; Check for a positive num
+    [(< n 1) (displayln "You Did not Enter a positive number!")]
+    ; Check when number is 1
+    [(= n 1)(cons n '())] 
+    ; Keep adding to the list using recursion until the number is 1
+    [(> n 1)(cons n(collatz-list (even-odd n)))]))
 
 ; Test Function with Set Numbers
 (collatz-list 5)
