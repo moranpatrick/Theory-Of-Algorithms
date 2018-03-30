9b36f5917785fc426c75252bdbd2bb9110f06472
moranpatrick <patrickmoran121@gmail.com>
2018-03-30 14:41:20 +0100

    Coded up Task 8 Solution

--- Tasks/Task8/Task8.rkt
+++ Tasks/Task8/Task8.rkt
@@ -2,8 +2,21 @@
 
 ; Define our chse function
 (define (chse x y z)
-  #t
+  (cond
+    ; If x, y or z is null then return an empty list
+    [(null? x)'()] 
+    [(null? y)'()]
+    [(null? y)'()]
+    ; Check if the first element of x is 1
+    [ (if(= (car x) 1)
+         ; If it is = 1, cons the first element of y to the new list and recurse on the cdr of x y z
+         (cons(car y)(chse(cdr x)(cdr y)(cdr z)))
+         ; If its not = 1, cons the first element of z to the new list and recurse on the cdr of x y z
+         (cons(car z)(chse(cdr x)(cdr y)(cdr z))))]
+    )
   )
 
 ; Test Cases
-;(define (chse (list) (list) (list)))
+(chse (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
+(chse (list 0 1 0 1 0 1 0 1) (list 1 1 1 1 0 0 1 1) (list 1 1 0 1 0 0 0 1))
+(chse (list 0 1 0 1 0 1 0 1) '() '())
